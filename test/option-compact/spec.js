8ad101803ed775c55a95f8ad645e4e79896ba976
Bart Veneman <bartveneman@users.noreply.github.com>
2019-03-04 22:36:36 +0100

    use AVA snapshot testing

--- test/option-compact/spec.js
+++ test/option-compact/spec.js
@@ -1,45 +1,26 @@
-const {promisify} = require('util')
-const fs = require('fs')
 const test = require('ava')
 const execa = require('execa')
 
-const readFile = promisify(fs.readFile)
-
 test(`it shows a compact table if the --compact option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['--compact'], {
-			input: 'a{}'
-		}),
-		readFile('./test/option-compact/expected.txt', {
-			encoding: 'utf8'
-		})
-	])
+	const {stdout} = await execa('./cli.js', ['--compact'], {
+		input: 'a{}'
+	})
 
-	t.deepEqual(actual, expected)
+	t.snapshot(stdout)
 })
 
 test(`it shows a compact table if the -c option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['-c'], {
-			input: 'a{}'
-		}),
-		readFile('./test/option-compact/expected.txt', {
-			encoding: 'utf8'
-		})
-	])
+	const {stdout} = await execa('./cli.js', ['-c'], {
+		input: 'a{}'
+	})
 
-	t.deepEqual(actual, expected)
+	t.snapshot(stdout)
 })
 
 test(`it shows all stats in the table if the --no-compact option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['--no-compact'], {
-			input: 'a{}'
-		}),
-		readFile('./test/no-options/expected.txt', {
-			encoding: 'utf8'
-		})
-	])
+	const {stdout} = await execa('./cli.js', ['--no-compact'], {
+		input: 'a{}'
+	})
 
-	t.deepEqual(actual, expected)
+	t.snapshot(stdout)
 })
--- test/option-format/spec.js
+++ test/option-format/spec.js
@@ -1,64 +1,32 @@
-const {promisify} = require('util')
-const fs = require('fs')
 const test = require('ava')
 const execa = require('execa')
 
-const readFile = promisify(fs.readFile)
-
 test(`it shows json output when the --format=json option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['--format', 'json'], {
-			input: 'a{}'
-		}),
-		readFile('./test/option-format/expected.json', {
-			encoding: 'utf8'
-		})
-	])
+	const {stdout} = await execa('./cli.js', ['--format', 'json'], {
+		input: 'a{}'
+	})
 
-	// Expected needs to trimmed because editors keep adding a
-	// newline to the end of the file :|
-	t.deepEqual(actual, expected.trim())
+	t.snapshot(stdout)
 })
 
 test(`it shows json output when the -f=json option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['-f', 'json'], {
-			input: 'a{}'
-		}),
-		readFile('./test/option-format/expected.json', {
-			encoding: 'utf8'
-		})
-	])
-
-	// Expected needs to trimmed because editors keep adding a
-	// newline to the end of the file :|
-	t.deepEqual(actual, expected.trim())
+	const {stdout} = await execa('./cli.js', ['-f', 'json'], {
+		input: 'a{}'
+	})
+	t.snapshot(stdout)
 })
 
 test(`it shows json output when an uppercase json format option is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['--format', 'JSON'], {
-			input: 'a{}'
-		}),
-		readFile('./test/option-format/expected.json', {
-			encoding: 'utf8'
-		})
-	])
-
-	// Expected needs to trimmed because editors keep adding a
-	// newline to the end of the file :|
-	t.deepEqual(actual, expected.trim())
+	const {stdout} = await execa('./cli.js', ['--format', 'JSON'], {
+		input: 'a{}'
+	})
+	t.snapshot(stdout)
 })
 
 test(`it shows pretty output when --format=pretty is passed`, async t => {
-	const [{stdout: actual}, expected] = await Promise.all([
-		execa('./cli.js', ['--format', 'pretty'], {
-			input: 'a{}'
-		}),
-		readFile('./test/no-options/expected.txt', {
-			encoding: 'utf8'
-		})
-	])
+	const {stdout} = await execa('./cli.js', ['--format', 'pretty'], {
+		input: 'a{}'
+	})
 
-	t.deepEqual(actual, expected)
+	t.snapshot(stdout)
 })

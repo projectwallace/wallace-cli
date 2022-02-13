82eba486c7f50b05bbcf35e027512a447b3513ae
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2022-02-13 12:25:35 +0100

    clean up before starting v3

--- test/version/spec.js
+++
@@ -1,8 +0,0 @@
-const test = require('ava')
-const execa = require('execa')
-const semver = require('semver')
-
-test('it shows a valid version when passing --version', async t => {
-	const {stdout: actual} = await execa('./src/cli.js', ['--version'])
-	t.is(actual, semver.valid(actual))
-})

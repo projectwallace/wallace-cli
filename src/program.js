959b01f2d89751f435af7dca3c0da77a3b955456
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2025-06-23 18:44:36 +0200

    breaking: use Node `parseArgs` for parsing CLI arguments

--- src/program.js
+++ src/program.js
@@ -1,26 +1,34 @@
 import { analyze } from '@projectwallace/css-analyzer'
+import { parseArgs } from 'node:util'
 import { help } from './help.js'
 import { Analytics } from './components.js'
 
 export async function Program({ args, read_file, terminal_colors, stdin }) {
-  const format_as_json = '--json'
-  const help_args = ['-h', '--help']
+  const options = {
+    json: {
+      type: 'boolean',
+      short: 'j'
+    },
+    help: {
+      type: 'boolean',
+      short: 'h'
+    }
+  }
 
-  // Show help if the user explicitly asked for it
-  if (args.some(arg => help_args.includes(arg))
-    // Show help if there's no input and no arguments provided
-    || args.length === 0 && stdin === '') {
+  const { values, positionals } = parseArgs({
+    args,
+    options,
+    allowPositionals: true,
+    strict: false
+  })
+
+  // Show help if the user explicitly asked for it or if no arguments were provided
+  if (values.help || args.length === 0 && stdin === '') {
     return help(terminal_colors)
   }
 
-  // path is the first param that doesn't start with -- and isn't one
-  // of the existing flags
-  const path_param = args.find(arg => {
-    if (arg == format_as_json) return false
-    if (help_args.includes(arg)) return false
-    if (arg.startsWith('--')) return false
-    return true
-  })
+  // Use the first positional argument as the file path
+  const path_param = positionals[0]
   const css = path_param ? await read_file(path_param) : stdin
 
   if (!css) {
@@ -31,7 +39,7 @@ export async function Program({ args, read_file, terminal_colors, stdin }) {
   delete stats.__meta__
 
   // Format as JSON if user asked for it
-  if (args.some(arg => arg === format_as_json)) {
+  if (values.json) {
     return JSON.stringify(stats)
   }
 
